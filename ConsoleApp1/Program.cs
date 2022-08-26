d3d7b1dfdf8c32558f9e1177535a777680a0a6d9
Patricio Scida <patricioscida@gmail.com>
2022-08-26 19:30:29 -0300

    Terminado punto 4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+using static ConsoleApp1.Provincias;
 
 namespace ConsoleApp1 {
     class Program {
@@ -22,6 +27,38 @@ namespace ConsoleApp1 {
             } catch (Exception e) {
                 Console.WriteLine("Error: " + e.Message);
             }
+            // PROBLEMA 3
+            try {
+                Console.WriteLine("Ingrese los km recorridos");
+                int kmRecorridos= Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese los litros usados");
+                int ltUsados = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Resultado: " + CalcularDivision(kmRecorridos, ltUsados));
+            } catch (Exception e) {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            // PROBLEMA 4
+            var url = $"https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+            try {
+                WebResponse response = request.GetResponse();
+                Stream strReader = response.GetResponseStream();
+                StreamReader ObjReader = new StreamReader(strReader);
+                string datos = ObjReader.ReadToEnd();
+                Root listProvincias = JsonSerializer.Deserialize<Root>(datos);
+                int i = 1;
+                foreach (Provincia datosProvincias in listProvincias.provincias) {
+                    Console.WriteLine("Nombre provincia N°" + i + ": " + datosProvincias.nombre);
+                    i++;
+                }
+                Console.WriteLine("hola");
+            } catch (Exception e) {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
         }
 
         public static int CalcularCuadrado(int a) {
