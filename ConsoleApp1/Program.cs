46385e35749207f1d9e93430bfc57740c17dd171
Patricio Scida <patricioscida@gmail.com>
2022-08-26 18:23:46 -0300

    Terminado Problema 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,7 +3,18 @@
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            // PROBLEMA 1
+            try {
+                Console.WriteLine("Ingrese un número para encontrar su cuadrado:");
+                int numeroIngresado = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Resultado: " + CalcularCuadrado(numeroIngresado));
+            } catch(Exception e) {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        public static int CalcularCuadrado(int a) {
+            return a * a;
         }
     }
 }
