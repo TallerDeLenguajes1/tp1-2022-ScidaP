c222ac6799cec1727c6ad674aa1794373c9961fe
Patricio Scida <patricioscida@gmail.com>
2022-08-26 18:33:16 -0300

    Punto 2) Problema 2 terminado

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
+            /////////// PUNTO 2)
             // PROBLEMA 1
             try {
                 Console.WriteLine("Ingrese un número para encontrar su cuadrado:");
@@ -11,10 +12,24 @@ namespace ConsoleApp1 {
             } catch(Exception e) {
                 Console.WriteLine("Error: " + e.Message);
             }
+            // PROBLEMA 2
+            try {
+                Console.WriteLine("Ingrese el dividendo");
+                int primerNumero = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese el divisor");
+                int segundoNumero = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Resultado: " + CalcularDivision(primerNumero, segundoNumero));
+            } catch (Exception e) {
+                Console.WriteLine("Error: " + e.Message);
+            }
         }
 
         public static int CalcularCuadrado(int a) {
             return a * a;
+        }
+
+        public static float CalcularDivision(int a, int b) {
+            return a / b;
         }
     }
 }
